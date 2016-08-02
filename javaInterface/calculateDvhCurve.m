6e216b906373a3b86bb5873376d60764e68ab401
Tim Hendriks <tim.hendriks@maastro.nl>
2016-08-02 12:07:18 +0200

    DIU-81 #comment Changed calculateDVHCurve to return a dvh curve object so that we can use it later on. #time 10m

--- javaInterface/calculateDvhCurve.m
+++ javaInterface/calculateDvhCurve.m
@@ -1,16 +1,7 @@
 %%CALCULATEDVHCURVE
-function [ vVolume, vDose ] = calculateDvhCurve(doseCube, binSize, relative)
-    %% input parsing
-    if ~isnumeric(doseCube)
-        throw(MException('calculateDvhCurve:InputTypeMismatch','doseCube should be numeric matrix'));
-    end
-    
+function gtv1Dvh = calculateDvhCurve(doseCube, binSize, relative)   
     if ~isnumeric(binSize) && length(binSize) == 1
         throw(MException('calculateDvhCurve:InputTypeMismatch','binSize should be single numeric value'));
-    end
-    
-    if ~isnumeric(pixelSpacing)
-        throw(MException('calculateDvhCurve:InputTypeMismatch','pixelSpacing should be numeric array'));
     end
     
     if ~islogical(relative) && length(relative) == 1
@@ -20,17 +11,8 @@ function [ vVolume, vDose ] = calculateDvhCurve(doseCube, binSize, relative)
     %% processing
     try 
         gtv1Dvh = DoseVolumeHistogram(doseCube, binSize);
-        vDose = gtv1Dvh.vDose;
-        
-        if relative
-            vVolume = gtv1Dvh.vVolumeRelative;
-        else
-            vVolume = gtv1Dvh.vVolume;
-        end
     catch EM
         warning('calculateDvhCurve:calculationError', EM.message);
-        vVolume = NaN;
-        vDose   = NaN;
     end
 end
 
