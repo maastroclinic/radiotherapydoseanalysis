6cdd1e2109899492325707c084248c263310e935
Tim Hendriks <tim.hendriks@maastro.nl>
2016-08-04 13:42:52 +0200

    DIU-81 #comment Added matlab function to combine two structs as it seems impossible in java. #time 5m.

---
+++ javaInterface/createDataStructure.m
@@ -0,0 +1,5 @@
+function outputStruct = createDataStructure(voimap, referenceDose)    
+    outputStruct = [];
+    outputStruct.voimap = voimap;
+    outputStruct.referenceDose = referenceDose;
+end
